97a9525820ef3d839541efba6ecb020792a02c60
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2024-04-15 12:41:56 +0530

    Make the backend api of the login

--- signup.js
+++ signup.js
@@ -1,27 +1,52 @@
-const SignUp = require('../models/signup');
+const SignUp = require("../models/signup");
+const bcrypt = require('bcrypt');
 
-exports.getsignup = (req, res, next) => {
-    res.render('signup');
+function stringValidator(string) {
+  if (string == undefined || string.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-exports.postsignup = (req, res, next) => {
-    const signup = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    SignUp.create(signup).then((exp) => {
-        console.log(exp);
-        res.redirect('/addsignup');
-    }).catch((err) => {
-        console.log(err);
+exports.postsignup = async (req, res, next) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    if (
+      stringValidator(name) ||
+      stringValidator(email) ||
+      stringValidator(password)
+    ) {
+      return res.json({ message: "Something is missing" });
+    } 
+    await SignUp.findAll({where: {email: email}}).then((data) => {
+      if (data.length === 1) {
+        res.status(201).json({message: "User already exist"});
+      }
+        await SignUp.create({name, email, password: hash})
+          res.status(201).json({message: "New user Created"});
     })
-}
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-exports.getpostsignup = (req, res, next) => {
-    SignUp.findAll().then((users) => {
-        console.log(users);
+exports.postlogin = async (req, res, next) => {
+  const password = req.body.password;
+    await SignUp.findAll({where: {email: req.body.email}}).then((data) => {
+      if (data.length === 0) {
+        return res.status(201).json({success: false, message: "User not found"});
+      }
+      if (password === data[0].password) {
+        if (!err){
+          res.status(201).json({success: true, message: "User login successful"});
+        } else {
+          return res.status(201).json({success: false, message: "User not authorized"});
+        }
+      }
     }).catch((err) => {
-        console.log(err);
-    });
-}
+      console.log(err);
+    })
+};
