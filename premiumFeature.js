0042aad86c59a5d559266b1255118ae5809f1989
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2024-05-14 00:45:36 +0530

    Final Leaderboard Optimisation

--- premiumFeature.js
+++ premiumFeature.js
@@ -5,30 +5,17 @@ const sequelize = require('../util/database');
 exports.showleaderboard = async (req, res, next) => {
     try {
         const users = await SignUp.findAll({
-            attributes: ['id', 'name']
+            attributes: ['id', 'name', [sequelize.fn('sum', sequelize.col('amount')), 'totalAmount']],
+            include: [
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group: ['signup.id'],
+            order: [['totalAmount', 'DESC']]
         });
-        const expenses = await Expense.findAll({
-            attributes: ['signupId', [sequelize.fn('sum', sequelize.col('expense.amount')), 'totalAmount']],
-            group: ['signupID']
-        });
-        const list = {};
-        console.log(expenses)
-
-        // expenses.forEach((expense) => {
-
-        //     if (list[expense.signupId]) {
-        //         list[expense.signupId] += expense.amount
-        //     }
-        //     else {
-        //         list[expense.signupId] = expense.amount
-        //     }
-        // })
-        var userLeaderboardDetails = [];
-        users.forEach((user) => {
-            userLeaderboardDetails.push({ name: user.name, totalExpense: list[user.id] || 0})
-        })  
-        userLeaderboardDetails.sort((a, b) => b.totalExpense - a.totalExpense)
-        res.status(200).json(expenses);
+        res.status(200).json(users);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
